235a4521c1744f47799bc62f18026e9413eba714
MusaeM <mustafa.marouf@hotmail.com>
2021-12-10 15:30:01 +0100

    Initial commit

---
+++ src/test/java/com/musaem/ToDoList/ToDoListApplicationTests.java
@@ -0,0 +1,13 @@
+package com.musaem.ToDoList;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ToDoListApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
